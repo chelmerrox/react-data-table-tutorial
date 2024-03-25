86239abec01c617598e0bd8d62cd6795fab06567
Losalini Rokocakau <c.rokocakau@gmail.com>
2024-03-26 00:47:12 +1200

    Add the react-data-table-component source code

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Table from "./components/Table";
+
+function App() {
+  return (
+    <>
+      <Table />
+    </>
+  );
+}
+
+export default App;
